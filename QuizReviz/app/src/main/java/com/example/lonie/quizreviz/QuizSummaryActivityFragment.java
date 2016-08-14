7af622427fab77dbac89e47e3671bcd003677145
leonieNd <leonie.ndoye@hotmail.com>
2016-08-14 14:22:57 +0200

    image et module Quiz

---
+++ QuizReviz/app/src/main/java/com/example/lonie/quizreviz/QuizSummaryActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.lonie.quizreviz;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class QuizSummaryActivityFragment extends Fragment {
+
+    public QuizSummaryActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_quiz_summary, container, false);
+    }
+}
